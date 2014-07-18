661355f9877639c74522562fe70784ae37f857c0
Adam Green <adam@bristlebird.com>
2014-07-18 17:43:25 +0100

    Changed h1 logo to svg

--- inc/nav.inc.php
+++ inc/nav.inc.php
@@ -1,4 +1,4 @@
-<a href="../index.php" title="Dunowen House"><h1>Dunowen House</h1></a>
+<a href="../index.php" title="Dunowen House"><h1>Dunowen House <span>Ardfield</span></h1></a>
 <a id="jump-top" href="#menu" class="menu-link ">&#9776; Menu</a>
 <nav id="main-nav" roll="navigation" class="main-nav">
 
