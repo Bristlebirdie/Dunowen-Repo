2f3d5183bb4b7f1149058eef18e8bed43903303f
Adam Green <adam@bristlebird.com>
2014-07-25 02:29:44 +0100

    clean urls - static pages

--- rates/index.php
+++ rates/index.php
@@ -16,7 +16,18 @@
        			<?php perch_content('Slideshow'); ?>				
 				</div><!--end gallery-->	
 	        	<div class="container">
-			        <?php perch_content('Content'); ?>
+					<?php perch_content('Page Introduction'); ?>
+					<div class="calendar-container">
+					<iframe src="https://www.google.com/calendar/embed?src=dunowenguesthouse%40gmail.com&ctz=Europe/Dublin" style="border: 0" width="100%" height="350" frameborder="0" scrolling="no"></iframe>
+					</div>
+					<?php
+						perch_content('Self Catering Table');			        
+					    perch_content('Heading');
+					    perch_content_custom('Products', array(
+					     'template'   => 'bandb.html',
+					     'multiple'    => true
+					    ));
+					?>
 			        <?php perch_content('Additional Content'); ?>
 				</div><!--end container-->
 				<?php include($_SERVER['DOCUMENT_ROOT'].'/inc/news.inc.php'); ?>
