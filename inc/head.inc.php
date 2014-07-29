6ab945016a957c41cdd93f75f3a3034e07162225
Adam Green <adam@bristlebird.com>
2014-07-29 17:43:33 +0100

    margin on main blog post image,fix functions include in head

--- inc/head.inc.php
+++ inc/head.inc.php
@@ -1,4 +1,4 @@
-<?php include('inc/functions.inc.php'); ?>
+<?php include('functions.inc.php'); ?>
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
