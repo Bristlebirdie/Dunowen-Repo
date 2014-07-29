794c9b182c9badd57acf4c339f833e0c6cf1af03
Adam Green <adam@bristlebird.com>
2014-07-29 15:18:11 +0100

    fix submenu disappearing on subpage

--- inc/nav.inc.php
+++ inc/nav.inc.php
@@ -1,25 +1,16 @@
-<a href="../index.php" title="Dunowen House"><h1>Dunowen House <span>Ardfield</span></h1></a>
+<a href="../" title="Dunowen House"><h1>Dunowen House <span>Ardfield</span></h1></a>
 <a id="jump-top" href="#menu" class="menu-link ">&#9776; Menu</a>
-<nav id="main-nav" roll="navigation" class="main-nav">
-
-	<?php perch_pages_navigation(array
-	(
-	       'template' => array('item.html', 'subitem.html')
-	)); 
-	?>
-	
-	<?php 
-	if(strpos($_SERVER['REQUEST_URI'], 'post.php')===false && strpos($_SERVER['REQUEST_URI'], 'archive.php')===false) {
-	
-		perch_pages_navigation(array
-		(
-				'from-path'=> '*',
-				'from-level'=> 2,
-				'template'=> 'second.html'				
+<nav id="main-nav" role="navigation" class="main-nav">
+<?php 
+	perch_pages_navigation(array(
+		'template' => array('item.html', 'subitem.html')
+	));
+	if(strpos($_SERVER['REQUEST_URI'], '/news/')===false && strpos($_SERVER['REQUEST_URI'], '/special-offers/')===false) {
+		perch_pages_navigation(array(
+			'from-path'=> '*',
+			'from-level'=> 1,
+			'template'=> 'second.html'				
 		)); 
-
 	}
-
-	?>
-
+?>
 </nav>
