85113b16a4618daa300f3dbf73b643699b19ec0a
Adam Green <adam@bristlebird.com>
2014-08-02 17:46:20 +0100

    add seo attributes

--- inc/headblog.inc.php
+++ inc/headblog.inc.php
@@ -4,15 +4,14 @@
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 <head>
-    <meta charset="UTF-8"/>
-	<title><?php perch_blog_post_field(perch_get('s'), 'postTitle'); ?> | Dunowen House Blog</title>   
-<!--    <base href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/"> -->
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="<?php echo $description; ?>">
-    <meta name="viewport" content="width=device-width">
-    <?php echo $meta_og; ?>
-    <link rel="stylesheet" href="<?php $_SERVER['DOCUMENT_ROOT'] ?>/css/style.css"/>
-    <link rel="stylesheet" href="<?php $_SERVER['DOCUMENT_ROOT'] ?>/css/menu.css" />
-    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic' rel='stylesheet' type='text/css'>
-    <link rel="shortcut icon" type="image/x-icon" href="../img/favicon.ico">
+<meta charset="UTF-8"/>
+<title><?php perch_blog_post_field(perch_get('s'), 'postTitle'); ?> | Dunowen House Blog</title>   
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="<?php echo $description; ?>">
+<meta name="viewport" content="width=device-width">
+<?php echo $meta_og; ?>
+<link rel="stylesheet" href="<?php $_SERVER['DOCUMENT_ROOT'] ?>/css/style.css"/>
+<link rel="stylesheet" href="<?php $_SERVER['DOCUMENT_ROOT'] ?>/css/menu.css" />
+<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic' rel='stylesheet' type='text/css'>
+<link rel="shortcut icon" type="image/x-icon" href="../img/favicon.ico">
 </head>   
