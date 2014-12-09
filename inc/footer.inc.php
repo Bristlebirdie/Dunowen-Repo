56289e297327f210db6fef17d4782ad5f60f11f0
Adam Green <adam@bristlebird.com>
2014-12-09 18:03:05 +0000

    fix facebook page link

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -7,7 +7,7 @@
 	</div><!--end address-->
 	
 	<div class="social">
-	    <a class="facebook" href="https://www.facebook.com/pages/Dunowen-House/191592117532570" target="_blank" title="Facebook">facebook</a>
+	    <a class="facebook" href="https://www.facebook.com/dunowenhouse" target="_blank" title="Facebook">facebook</a>
 	    <a class="twitter" href="https://twitter.com/dunowenhouse" target="_blank" title="twitter">twitter</a>
 	    <a class="tripadvisor" href="http://www.tripadvisor.ie/VacationRentalReview-g211877-d2053918-Dunowen_House-Clonakilty_County_Cork.html" target="_blank" title="tripadvisor">tripadvisor</a>
 	</div><!--end social-->
