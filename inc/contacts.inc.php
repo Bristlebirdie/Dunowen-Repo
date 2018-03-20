2a3b1efc757c9b93e418f98ec0411675baed58da
Adam Green <adam@bristlebird.com>
2018-03-20 17:53:31 +0000

    update guides logo in sidebar

--- inc/contacts.inc.php
+++ inc/contacts.inc.php
@@ -12,8 +12,8 @@
         <a href='m&#97;ilto&#58;in%66&#111;&#64;d&#37;&#55;5nowen&#104;&#111;&#117;%7&#51;e&#46;&#105;&#37;65'>in&#102;o&#64;dun&#111;wenhouse&#46;ie</a></p>
     </div>
     <div class="tourism-logos">
-       	<div class="logo guides-2016"><a href="http://www.guides.ie/recommendations/dunowen-house">McKenna's Guides Best in Ireland 2016</a></div>
-    	<div class="logo guides-2017"><a href="http://www.guides.ie/recommendations/dunowen-house">McKenna's Guides Best in Ireland 2017</a></div>
+       	<div class="logo guides-2018"><a href="http://www.guides.ie/recommendations/dunowen-house">McKenna's Guides Best in Ireland 2016</a></div>
+    	<!--<div class="logo guides-2017"><a href="http://www.guides.ie/recommendations/dunowen-house">McKenna's Guides Best in Ireland 2017</a></div>-->
     	<!--<div class="logo failte-ireland"><a href="http://failteireland.ie">Failte Ireland</a></div>-->
     	<div class="logo wild-atlantic-way"><a href="http://wildatlanticway.com/">Wild Atlantic Way</a></div>
     </div>
