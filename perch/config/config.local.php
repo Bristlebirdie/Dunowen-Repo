7597b9105dedd8e9360512bfa0788af0078ac91e
lucie green <lucie@bristlebird.com>
2018-03-21 14:39:44 +0000

    local config changes
    Signed-off-by: lucie green <lucie@bristlebird.com>

--- perch/config/config.local.php
+++ perch/config/config.local.php
@@ -1,9 +1,9 @@
 <?php
 
-	define('PERCH_SITEPATH', '/Users/adam/Sites/dunowenhouse.ie/site');
+	define('PERCH_SITEPATH', '/Users/Lucies-iMac-2/Sites/dunowenhouse.ie/site');
 //	define('PERCH_SCHEDULE_SECRET', 'uTlNeCOMMhE5WrYq');
 
 	define('PERCH_DB_USERNAME', 'root');
-	define('PERCH_DB_PASSWORD', 'cr4zapHe');
-	define('PERCH_DB_DATABASE', 'dunowenh_db');
+	define('PERCH_DB_PASSWORD', 'bt636dj');
+	define('PERCH_DB_DATABASE', 'dunowenhouse_db');
 	define('PERCH_PRODUCTION_MODE', PERCH_DEVELOPMENT);
